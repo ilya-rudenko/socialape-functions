d78708ed8ac966198512a12df1ecb1b1bfeede12
Ilya <skyrimfornord337@gmail.com>
2021-05-25 20:10:38 +0500

    Added firebase functions

---
+++ dbschema.js
@@ -0,0 +1,20 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2021-05-04T12:06:29.638Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
+
+const userDetails = {
+  //Redux data
+  credentials: {
+    userId: "FKd3JcssS0alHGQTsOOwq0AocW12",
+    email: "user@email.com",
+    handle: "user",
+  },
+};
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBjJpZnsEdza3ovNFmnwSaly4HjclsdcJA",
+  authDomain: "rudenko-socialape.firebaseapp.com",
+  databaseURL: "https://rudenko-socialape-default-rtdb.firebaseio.com",
+  projectId: "rudenko-socialape",
+  storageBucket: "rudenko-socialape.appspot.com",
+  messagingSenderId: "337782498242",
+  appId: "1:337782498242:web:a8fa1d50d4de8c43e3ec5b",
+  measurementId: "G-2F43HFR46P",
+};
